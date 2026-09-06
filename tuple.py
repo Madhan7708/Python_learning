d3586dfe16c7fc5638fdfa64f992c8946f7b6ead
Madhan7708 <bashamadhan002@gmail.com>
2026-09-06 11:49:56 +0530

    tuple  operation done success

---
+++ tuple.py
@@ -0,0 +1,17 @@
+tuple=(1,2,3,4,5)
+tuple1=(6,7,8,9,10,10,2)
+tuple=tuple+tuple1
+
+print(tuple)
+# print(len(tuple))
+
+# print(sum(tuple))
+# print(tuple.count(10))
+# print(tuple.index(10))
+
+#tuple to list
+my_tuple = (10, 20, 30, 40)
+
+my_list = list(my_tuple)
+
+print(my_list)
